de1b8a698d31ba9764a42e4a01c53d81eef07b74
Jakr Hung <lhhungx@gmail.com>
2025-07-24 02:29:31 +0800

    Add code of 10-sync (#10)

---
+++ 10-sync/murky/mysync_test.go
@@ -0,0 +1,42 @@
+package mysync
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestCounter(t *testing.T) {
+	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		assertCounter(t, &counter, 3)
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for range wantedCount {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+
+		wg.Wait() // 一定要Mutex不然不會過
+		assertCounter(t, &counter, wantedCount)
+	})
+}
+
+func assertCounter(t testing.TB, got *Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("got %d, want %d", got.Value(), want)
+	}
+}
